d45dea9861dfb893f262a258a8c375047045ce67
Michelle Z <mzulli@users.noreply.github.com>
2021-08-23 16:13:42 -0400

    first assignment

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "USA";
+let continent = "North America";
+let population = "332,915,073";
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -16,7 +16,7 @@ let $function = 27;
 let person = "jonas";
 let PI = 3.1415;
 
-let myFirstJob = "Programmer";
+let myFirstJob = "Coder";
 let myCurrentJob = "Teacher";
 
 let job1 = "programmer";
