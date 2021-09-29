c7efc62baa07a1578e5cb9b736d7da9cbf150a7e
Michelle Z <mzulli@users.noreply.github.com>
2021-09-29 15:26:31 -0400

    Update

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,10 @@
 let country = "USA";
 let continent = "North America";
 let population = "332,915,073";
+let isIsland = false;
+let language;
 
-console.log(country);
-console.log(continent);
-console.log(population);
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
